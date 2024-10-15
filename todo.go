4f46a96ac419c0ce27e36a5a7f64f9aeb6992f32
Chathusha Mendis <chathushamendis@icloud.com>
2024-10-15 21:41:06 +0530

    toggle method

--- todo.go
+++ todo.go
@@ -46,3 +46,22 @@ func (todos *Todos) delete(index int) error {
 
 	return nil
 }
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+	}
+
+	t[index].Completed = !isCompleted
+
+	return nil
+}
