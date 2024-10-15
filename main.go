c0ddf037940333a057ddc0b5acd2b7292d1aa40f
Chathusha Mendis <chathushamendis@icloud.com>
2024-10-15 21:59:12 +0530

    test print method

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 	todos.add("Buy milk")
 	todos.add("Buy bread")
-	fmt.Printf("%+v\n\n", todos)
-	err := todos.delete(0)
+	err := todos.toggle(0)
 	if err != nil {
 		return
 	}
-	fmt.Printf("%+v\n\n", todos)
+	todos.print()
 }
